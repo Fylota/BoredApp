d5d96ec1c343f830603e35ca0fb740df82b0ba0f
Ágota Fülöp <fulop.agota01@gmail.com>
2023-05-07 20:18:16 +0200

    Updated Api Service with GET request

--- app/src/main/java/com/example/boredapp/network/BoredActivityService.kt
+++ app/src/main/java/com/example/boredapp/network/BoredActivityService.kt
@@ -1,4 +1,11 @@
 package com.example.boredapp.network
 
+import com.example.boredapp.model.BoredActivity
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface BoredActivityService {
+    @GET("/activity")
+    suspend fun getRandomActivity(@Query("key") key: Long?): Response<BoredActivity>
 }
